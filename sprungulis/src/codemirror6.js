d73c210379a9f7377cb955ad5ba15ce010af8f48
JanisLapa44 <nilsbriska444@gmail.com>
2026-02-15 12:46:01 +0200

    Restore files from before deletion

---
+++ sprungulis/src/codemirror6.js
@@ -0,0 +1,59 @@
+// required dependencies: node.js , npm
+// required codemirror6 dependencies: npm install @codemirror/view @codemirror/state @codemirror/commands @codemirror/language @codemirror/lang-javascript
+// now stored in package, so can just run npm install and everything neccessary will download
+
+import React, { useEffect, useRef } from 'react';
+import { EditorView } from '@codemirror/view';
+import { basicSetup } from 'codemirror';
+import { EditorState } from '@codemirror/state';
+import { python } from '@codemirror/lang-python'; // ?
+
+
+function CodeEditor({onChange , initialDoc}) {
+    const editorRef = useRef(null);
+    const viewRef = useRef(null);
+
+    useEffect(() => {
+        if(!editorRef.current) return;
+        
+        const state = EditorState.create({
+            doc: '# raksti kodu šeit\nsveiki = "pasaule"\nprint(sveiki)',
+            extensions: [
+                basicSetup,
+                python(),
+                EditorView.updateListener.of((update) => {
+                    if (update.docChanged && typeof onChange === 'function') {
+                        onChange(update.state.doc.toString());
+                    }
+                })
+            ]
+        });
+
+        if (!typeof onChange === 'function') {
+            onChange(state.doc.toString());
+        }
+
+        const view = new EditorView({
+            state,
+            parent: editorRef.current
+        });
+
+        viewRef.current = view;
+
+        return () => {
+            view.destroy();
+        }
+    }, [initialDoc, onChange]);
+
+    return (
+        <div
+            ref={editorRef}
+            style={{
+                fontFamily: 'monospace'
+            }}
+        />
+    );
+}
+
+export default CodeEditor;
+
